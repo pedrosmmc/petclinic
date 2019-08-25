04ae7cc505aca9fe16ec970bb7abef5bb2681e2e
pedrosmmc <pedrosmmc@gmail.com>
2019-08-25 16:28:17 +0100

    Listing all Vets on Vets Index Page. Closes #16. Listing all Pets on Pets Index Page. Closes #18.

--- petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/controllers/PetController.java
+++ petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/controllers/PetController.java
@@ -1,12 +1,22 @@
 package com.pedrocoelho.learningspringframework.controllers;
 
+import com.pedrocoelho.learningspringframework.services.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PetController {
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
     @RequestMapping("/pets")
-    public String petsList() {
+    public String petsList(Model model) {
+        model.addAttribute("pets", petService.findAll());
+
         return "pets/index";
     }
 }
