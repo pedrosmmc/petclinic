ad49d4854aaced173ed3f4f7e4b0449bf174ec41
pedrosmmc <pedrosmmc@gmail.com>
2019-08-26 21:25:24 +0100

    Apply Master Layout to Owners List Page. Closes #22.

--- petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/controllers/IndexController.java
+++ petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/controllers/IndexController.java
@@ -1,16 +1,10 @@
 package com.pedrocoelho.learningspringframework.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
-    @Autowired
-    private MessageSource messageSource;
 
     @RequestMapping({"", "/", "/index", "index.html"})
     public String index() {
