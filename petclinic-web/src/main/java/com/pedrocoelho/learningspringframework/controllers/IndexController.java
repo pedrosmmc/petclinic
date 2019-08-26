05d0a14a46397c1b3d4c5274076c36ed3f62894f
pedrosmmc <pedrosmmc@gmail.com>
2019-08-26 20:40:17 +0100

    Add missing i18 properties files. Closes #27.

--- petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/controllers/IndexController.java
+++ petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/controllers/IndexController.java
@@ -1,10 +1,16 @@
 package com.pedrocoelho.learningspringframework.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @RequestMapping({"", "/", "/index", "index.html"})
     public String index() {
