5e6b28c674582994a217acb7defff1e0dfc10969
pedrosmmc <pedrosmmc@gmail.com>
2019-10-29 08:41:19 +0000

    Created Spring Data JPA Vet Service. Closes #43.

--- petclinic-data/src/main/java/com/pedrocoelho/learningspringframework/repositories/VetRepository.java
+++ petclinic-data/src/main/java/com/pedrocoelho/learningspringframework/repositories/VetRepository.java
@@ -3,5 +3,10 @@ package com.pedrocoelho.learningspringframework.repositories;
 import com.pedrocoelho.learningspringframework.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface VetRepository extends CrudRepository<Vet, Long> {
+    List<Vet> findAllByLastName(String lastName);
+
+    List<Vet> findAllByFirstName(String firstName);
 }
---
+++ petclinic-data/src/main/java/com/pedrocoelho/learningspringframework/services/dao/VetDao.java
@@ -0,0 +1,60 @@
+package com.pedrocoelho.learningspringframework.services.dao;
+
+import com.pedrocoelho.learningspringframework.model.Vet;
+import com.pedrocoelho.learningspringframework.repositories.SpecialityRepository;
+import com.pedrocoelho.learningspringframework.repositories.VetRepository;
+import com.pedrocoelho.learningspringframework.services.VetService;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class VetDao implements VetService {
+
+    private final VetRepository vetRepository;
+    private final SpecialityRepository specialityRepository;
+
+    public VetDao(VetRepository vetRepository, SpecialityRepository specialityRepository) {
+        this.vetRepository = vetRepository;
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public List<Vet> findAllByFirstName(String firstName) {
+        return vetRepository.findAllByFirstName(firstName);
+    }
+
+    @Override
+    public List<Vet> findAllByLastName(String lastName) {
+        return vetRepository.findAllByLastName(lastName);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        Optional<Vet> optionalVet = vetRepository.findById(id);
+        return optionalVet.orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet entity) {
+        return vetRepository.save(entity);
+    }
+
+    @Override
+    public void delete(Vet entity) {
+        vetRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
