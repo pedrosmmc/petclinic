fdf9c30082cf51e4b7e7d62fe95b971438bca3ec
pedrosmmc <pedrosmmc@gmail.com>
2019-11-16 19:40:27 +0000

    Displays Owner Record (HTML). Closes #57

--- petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/bootstrap/DataPopulate.java
+++ petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/bootstrap/DataPopulate.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class DataPopulate implements CommandLineRunner {
@@ -35,31 +37,43 @@ public class DataPopulate implements CommandLineRunner {
         PetType cat = new PetType();
         cat.setName("Cat");
 
-        petTypeService.saveAll(Arrays.asList(dog,cat));
+        petTypeService.saveAll(Arrays.asList(dog, cat));
 
         Pet p1 = new Pet();
         p1.setName("Sancho");
-        p1.setBirthDay(LocalDate.of(2000,1,13));
+        p1.setBirthDay(LocalDate.of(2000, 1, 13));
         p1.setPetType(dog);
         p1.setSex("Male");
 
         Pet p2 = new Pet();
         p2.setName("Georgina");
-        p2.setBirthDay(LocalDate.of(2019,8,26));
+        p2.setBirthDay(LocalDate.of(2019, 8, 26));
         p2.setPetType(cat);
         p2.setSex("Female");
 
         Pet p3 = new Pet();
         p3.setName("Gregory");
-        p3.setBirthDay(LocalDate.of(2017,2,6));
+        p3.setBirthDay(LocalDate.of(2017, 2, 6));
         p3.setPetType(cat);
         p3.setSex("Male");
 
         Pet p4 = new Pet();
         p4.setName("Fanny");
-        p4.setBirthDay(LocalDate.of(2017,2,6));
+        p4.setBirthDay(LocalDate.of(2017, 2, 6));
         p4.setPetType(dog);
         p4.setSex("Female");
+
+        Pet p5 = new Pet();
+        p5.setName("Pancho");
+        p5.setBirthDay(LocalDate.of(2013, 12, 26));
+        p5.setPetType(dog);
+        p5.setSex("Male");
+
+        Pet p6 = new Pet();
+        p6.setName("Buggy");
+        p6.setBirthDay(LocalDate.of(2018, 3, 16));
+        p6.setPetType(dog);
+        p6.setSex("Female");
 
         System.out.println("Populated pets...");
 
@@ -69,17 +83,22 @@ public class DataPopulate implements CommandLineRunner {
         o1.addPet(p1);
         p1.setOwner(o1);
 
+//        Set<Pet> pets = new HashSet<>(Arrays.asList(p4, p5, p6));
+//        Owner o2 = Owner.builder().firstName("Fiona").lastName("Martinez").pets(pets).build();
         Owner o2 = Owner.builder().firstName("Fiona").lastName("Martinez").build();
         o2.setAddress("Travessa do Galheiro, 44, Cucujães");
         o2.setPhoneNumber("255123234");
-        o2.addPet(p4);
+        o2.addPet(p4); // Fanny
         p4.setOwner(o2);
+        o2.addPet(p5); // Pancho
+        p5.setOwner(o2);
+        o2.addPet(p6); // Buggy
+        p6.setOwner(o2);
 
         Owner o3 = Owner.builder().firstName("Martha").lastName("Alvarez").build();
         o3.setAddress("Rua da Mesquita, 33, Los Cacos, Beja");
         o3.setPhoneNumber("123123123");
         o3.addPet(p2);
-//        o3.addPet(p3);
         p2.setOwner(o3);
 
 
@@ -91,7 +110,7 @@ public class DataPopulate implements CommandLineRunner {
         p3.setOwner(o4);
 
 
-        ownerService.saveAll(Arrays.asList(o1,o2,o3,o4));
+        ownerService.saveAll(Arrays.asList(o1, o2, o3, o4));
 
         System.out.println("Populated owners...");
 
@@ -118,25 +137,25 @@ public class DataPopulate implements CommandLineRunner {
         v2.addSpecialty(savedSurgery);
         v2.addSpecialty(savedDentistry);
 
-        vetService.saveAll(Arrays.asList(v1,v2));
+        vetService.saveAll(Arrays.asList(v1, v2));
 
         System.out.println("Populated vets...");
 
         Visit visit1 = new Visit();
         visit1.setPet(petService.findById(p1.getId()));
         visit1.setDescription("vacinar");
-        visit1.setDate(LocalDate.of(2019,11,20));
-        visit1.setTime(LocalTime.of(12,30));
+        visit1.setDate(LocalDate.of(2019, 11, 20));
+        visit1.setTime(LocalTime.of(12, 30));
 
-        Visit visit2 = new Visit(LocalDate.of(2019,11,29), LocalTime.of(9,45));
+        Visit visit2 = new Visit(LocalDate.of(2019, 11, 29), LocalTime.of(9, 45));
         visit2.setPet(p2);
         visit2.setDescription("fratura exposta");
 
-        Visit visit3 = new Visit(LocalDate.of(2020,1,16), LocalTime.of(16,30));
+        Visit visit3 = new Visit(LocalDate.of(2020, 1, 16), LocalTime.of(16, 30));
         visit3.setPet(p3);
         visit3.setDescription("tosquia");
 
-        Visit visit4 = new Visit(LocalDate.of(2019,11,20),LocalTime.of(12,0));
+        Visit visit4 = new Visit(LocalDate.of(2019, 11, 20), LocalTime.of(12, 0));
         visit4.setPet(p4);
         visit4.setDescription("remoção de quisto");
 
--- petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/controllers/OwnerController.java
+++ petclinic-web/src/main/java/com/pedrocoelho/learningspringframework/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package com.pedrocoelho.learningspringframework.controllers;
 import com.pedrocoelho.learningspringframework.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/owners")
 @Controller
@@ -25,5 +27,11 @@ public class OwnerController {
     public String findOwners() {
 
         return "not_implemented";
+    }
+
+    @RequestMapping("/{id}")
+    public String getOwnerById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("owner", ownerService.findById(id));
+        return "owners/single";
     }
 }
